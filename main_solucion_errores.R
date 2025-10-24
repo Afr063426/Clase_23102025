6dc8c9626b0aa42598ff8bd1c72c497df905bf5c
Afr063426 <afr0@Joshuas-MacBook-Air.local>
2025-10-23 19:00:15 -0600

    fix: solucionar error calculo promedio na

--- main_solucion_errores.R
+++ main_solucion_errores.R
@@ -7,8 +7,8 @@ datos <- data.frame(
 )
 
 # 1. Intentar calcular la media sin manejar NAs
-media_valor1 <- mean(datos$Valor1)
-print(paste("Media calculada (con NA):", media_valor1))
+media_valor1 <- mean(datos$Valor1, na.rm = TRUE)
+print(paste("Media calculada (sin NA):", media_valor1))
 
 # 2. Intentar una división que incluye un cero
 resultado_division <- datos$Valor1 / datos$Valor2
