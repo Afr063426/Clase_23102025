85f67eef3cd55317b9dfeab83d0317640488841f
Afr063426 <afr0@Joshuas-MacBook-Air.local>
2025-10-23 19:31:29 -0600

    feature: annade analisis de iris

--- main_graficos.R
+++ main_graficos.R
@@ -1,1 +1,25 @@
 #Nos piden hacer graficos
+
+# ============================
+# ------ Carga paquetes ------
+# ============================
+library(tidyverse)
+
+
+iris %>%
+  ggplot(aes(fill = Species, x = Petal.Width)) +
+  geom_density() +
+  theme_minimal()
+
+
+
+iris %>%
+  ggplot(aes(fill = Species, x = Petal.Length)) +
+  geom_density() +
+  theme_minimal()
+
+iris %>%
+  ggplot(aes(fill = Species, x = Sepal.Length)) +
+  geom_density() +
+  theme_minimal()
+
