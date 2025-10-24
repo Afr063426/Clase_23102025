fac18df087dd4cea888a228f834deab2b455065d
Afr063426 <afr0@Joshuas-MacBook-Air.local>
2025-10-23 18:52:08 -0600

    feature: mejora rendimiento en transformacion rgb a hex

--- main_vec.R
+++ main_vec.R
@@ -29,27 +29,19 @@ df <- data.frame("red" = sample(0:255, rows, replace = TRUE),
                  "blue" = sample(0:255, rows, replace = TRUE))
 
 
-#Veamo otro ejemplo 
-for(r in 1:nrow(df)) {
-  # get a value for each position in the hex code
-  # first pair
-  h1 <- hex[floor(df$red[r] / 16) + 1]
-  h2 <- hex[df$red[r] %% 16 + 1]
+
+df$hex <- paste0(
+  "#", 
+  hex[floor(df$red/16) + 1], 
+  hex[df$red %% 16 + 1], 
+  hex[floor(df$green / 16) + 1], 
+  hex[df$green %% 16 + 1], 
+  hex[floor(df$blue / 16) + 1], 
+  hex[df$blue %% 16 + 1]
+)
+
+df  
   
-  # second pair
-  h3 <- hex[floor(df$green[r] / 16) + 1]
-  h4 <- hex[df$green[r] %% 16 + 1]
-
-  # third pair
-  h5 <- hex[floor(df$blue[r] / 16) + 1]
-  h6 <- hex[df$blue[r] %% 16 + 1]
-  
-  # assemble the values using `paste0` and assign it to the `hex` column for 
-  # the corresponding row
-  df$hex[r] <- paste0("#", h1, h2, h3, h4, h5, h6)
-}
-
-
 ows <- 10^4
 hex <- c(0:9, LETTERS[1:6])
 
